2d9dc77eaa3a8b2be52455ad3ebc1f0e2eef417b
killsystem10 <killsystem10@outlook.com>
2023-12-22 11:50:49 +0900

    Add int scanner.java

---
+++ int scanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class scanner {
+    public static void main(String[] args) {
+        int a;
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("input int");
+        a=s.nextInt();
+        System.out.print(a);
+    }
+}
